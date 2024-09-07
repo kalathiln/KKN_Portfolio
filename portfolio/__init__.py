6e4a4cac512ee9e426e65cac7ebc6ed8abae77be
kalathiln <nikhkk@gmail.com>
2024-09-07 18:54:55 +0200

    Addition of Projects Page - Implementing CSS Grid and appropriate styling

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -3,13 +3,42 @@ from dotenv import load_dotenv
 # This is used to load environmanet variables from the .env file during deploytment.
 load_dotenv()
 
+projects = [
+    
+    {
+        "name" : "Photography in Abstract",
+        "thumb": "/img/headshot.png",
+        "hero": "",
+        "categories":["Abstract","Portrait", "Landscape"],
+        "slug": "habit-tracking",
+        "prod": "https://udemy.com",
+    },
+    {
+        "name" : "Germany",
+        "thumb": "/img/germany_subway.png",
+        "hero": "",
+        "categories":["Landscape","Wald" , "subway" ],
+        "slug": "germany",
+        "prod": "https://udemy.com",
+    },
+    {
+        "name" : "India",
+        "thumb": "/img/nandi_temple.png",
+        "hero": "",
+        "categories":["India","landscape"],
+        "slug": "api-docs",
+        "prod": "https://udemy.com",
+    }
+
+]
+
 def create_app():
     app = Flask(__name__)
 
     @app.route("/", methods=["GET", "POST"])
-    @app.route("/home", methods=["GET", "POST"])
+    @app.route("/home", methods=["GET", "POST"] )
     def home():
-        return render_template("home.html")
+        return render_template("home.html", projects = projects)
     
     @app.route("/about", methods=["GET", "POST"])
     def about():
