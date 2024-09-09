2292845a23f17ba3a0c85122ebc8b87eb3c3ae1b
kalathiln <nikhkk@gmail.com>
2024-09-09 06:04:18 +0200

    Added Static content to Photography pages : - Using lightbox to implement Photo display. - CSS grid to display all the Photograph. - Changed background with gradiants - Minor tweek in styling.

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort
 from dotenv import load_dotenv
 # This is used to load environmanet variables from the .env file during deploytment.
 load_dotenv()
@@ -10,7 +10,7 @@ projects = [
         "thumb": "/img/headshot.png",
         "hero": "",
         "categories":["Abstract","Portrait", "Landscape"],
-        "slug": "habit-tracking",
+        "slug": "abstract-photography",
         "prod": "https://udemy.com",
     },
     {
@@ -18,19 +18,31 @@ projects = [
         "thumb": "/img/germany_subway.png",
         "hero": "",
         "categories":["Landscape","Wald" , "subway" ],
-        "slug": "germany",
+        "slug": "germany-photography",
         "prod": "https://udemy.com",
     },
     {
         "name" : "India",
         "thumb": "/img/nandi_temple.png",
         "hero": "",
-        "categories":["India","landscape"],
-        "slug": "api-docs",
+        "categories":["India","people", "culture"],
+        "slug": "india-photography",
         "prod": "https://udemy.com",
+    },
+    {
+        "name" : "Shop",
+        "thumb": "/img/giraffe_small.png",
+        "hero": "",
+        "categories":["K&K", "Ganesha", "Psylosphy"],
+        "slug": "myshop",
+        "prod": "https://www.redbubble.com/de/shop/ap/162571374?asc=u",
     }
 
+    
+
 ]
+
+slug_to_project = {project["slug"]:project for project in projects}
 
 def create_app():
     app = Flask(__name__)
@@ -47,5 +59,14 @@ def create_app():
     @app.route("/contact")
     def contact():
         return render_template("contact.html")
+    
+    @app.route("/project/<string:slug>")
+    def project(slug):
+        if slug not in slug_to_project:
+            abort(404)
 
+        # return render_template("project_"+slug+".html")
+        return render_template(f"{slug}.html",
+                               project=slug_to_project[slug])
+        
     return app
